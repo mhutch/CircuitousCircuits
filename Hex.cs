42df9a4b52c8d4398c4b82f4d793955d95f2f321
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2019-03-15 11:01:24 -0500

    Port Red Blob hex code to Godot

--- Hex.cs
+++ Hex.cs
@@ -3,17 +3,7 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
-
-struct Point
-{
-    public Point(double x, double y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-    public readonly double x;
-    public readonly double y;
-}
+using Godot;
 
 struct Hex
 {
@@ -80,7 +70,7 @@ struct Hex
 
     public int Length()
     {
-        return (int)((Math.Abs(q) + Math.Abs(r) + Math.Abs(s)) / 2);
+        return (int)((Mathf.Abs(q) + Mathf.Abs(r) + Mathf.Abs(s)) / 2);
     }
 
 
@@ -93,25 +83,26 @@ struct Hex
 
 struct FractionalHex
 {
-    public FractionalHex(double q, double r, double s)
+    public FractionalHex(float q, float r, float s)
     {
-        this.q = q;
-        this.r = r;
-        this.s = s;
-        if (Math.Round(q + r + s) != 0) throw new ArgumentException("q + r + s must be 0");
+        Q = q;
+        R = r;
+        S = s;
+        if (Mathf.Round(q + r + s) != 0) throw new ArgumentException("q + r + s must be 0");
     }
-    public readonly double q;
-    public readonly double r;
-    public readonly double s;
+
+    public float Q { get; }
+    public float R { get; }
+    public float S { get; }
 
     public Hex HexRound()
     {
-        int qi = (int)(Math.Round(q));
-        int ri = (int)(Math.Round(r));
-        int si = (int)(Math.Round(s));
-        double q_diff = Math.Abs(qi - q);
-        double r_diff = Math.Abs(ri - r);
-        double s_diff = Math.Abs(si - s);
+        int qi = (int)(Mathf.Round(Q));
+        int ri = (int)(Mathf.Round(R));
+        int si = (int)(Mathf.Round(S));
+        float q_diff = Mathf.Abs(qi - Q);
+        float r_diff = Mathf.Abs(ri - R);
+        float s_diff = Mathf.Abs(si - S);
         if (q_diff > r_diff && q_diff > s_diff)
         {
             qi = -ri - si;
@@ -129,19 +120,19 @@ struct FractionalHex
     }
 
 
-    public FractionalHex HexLerp(FractionalHex b, double t)
+    public FractionalHex HexLerp(FractionalHex b, float t)
     {
-        return new FractionalHex(q * (1.0 - t) + b.q * t, r * (1.0 - t) + b.r * t, s * (1.0 - t) + b.s * t);
+        return new FractionalHex(Q * (1.0f - t) + b.Q * t, R * (1.0f - t) + b.R * t, S * (1.0f - t) + b.S * t);
     }
 
 
     static public List<Hex> HexLinedraw(Hex a, Hex b)
     {
         int N = a.Distance(b);
-        FractionalHex a_nudge = new FractionalHex(a.q + 0.000001, a.r + 0.000001, a.s - 0.000002);
-        FractionalHex b_nudge = new FractionalHex(b.q + 0.000001, b.r + 0.000001, b.s - 0.000002);
+        FractionalHex a_nudge = new FractionalHex(a.q + 0.000001f, a.r + 0.000001f, a.s - 0.000002f);
+        FractionalHex b_nudge = new FractionalHex(b.q + 0.000001f, b.r + 0.000001f, b.s - 0.000002f);
         List<Hex> results = new List<Hex> { };
-        double step = 1.0 / Math.Max(N, 1);
+        float step = 1.0f / Mathf.Max(N, 1f);
         for (int i = 0; i <= N; i++)
         {
             results.Add(a_nudge.HexLerp(b_nudge, step * i).HexRound());
@@ -160,8 +151,9 @@ struct OffsetCoord
     }
     public readonly int col;
     public readonly int row;
-    static public int EVEN = 1;
-    static public int ODD = -1;
+
+    public const int Even = 1;
+    public const int Odd = -1;
 
     static public OffsetCoord QoffsetFromCube(int offset, Hex h)
     {
@@ -245,7 +237,7 @@ struct DoubledCoord
 
 struct Orientation
 {
-    public Orientation(double f0, double f1, double f2, double f3, double b0, double b1, double b2, double b3, double start_angle)
+    public Orientation(float f0, float f1, float f2, float f3, float b0, float b1, float b2, float b3, float start_angle)
     {
         this.f0 = f0;
         this.f1 = f1;
@@ -257,76 +249,75 @@ struct Orientation
         this.b3 = b3;
         this.start_angle = start_angle;
     }
-    public readonly double f0;
-    public readonly double f1;
-    public readonly double f2;
-    public readonly double f3;
-    public readonly double b0;
-    public readonly double b1;
-    public readonly double b2;
-    public readonly double b3;
-    public readonly double start_angle;
+    public readonly float f0;
+    public readonly float f1;
+    public readonly float f2;
+    public readonly float f3;
+    public readonly float b0;
+    public readonly float b1;
+    public readonly float b2;
+    public readonly float b3;
+    public readonly float start_angle;
 }
 
 struct Layout
 {
-    public Layout(Orientation orientation, Point size, Point origin)
+    public Layout(Orientation orientation, Vector2 size, Vector2 origin)
     {
         this.orientation = orientation;
         this.size = size;
         this.origin = origin;
     }
     public readonly Orientation orientation;
-    public readonly Point size;
-    public readonly Point origin;
-    static public Orientation pointy = new Orientation(Math.Sqrt(3.0), Math.Sqrt(3.0) / 2.0, 0.0, 3.0 / 2.0, Math.Sqrt(3.0) / 3.0, -1.0 / 3.0, 0.0, 2.0 / 3.0, 0.5);
-    static public Orientation flat = new Orientation(3.0 / 2.0, 0.0, Math.Sqrt(3.0) / 2.0, Math.Sqrt(3.0), 2.0 / 3.0, 0.0, -1.0 / 3.0, Math.Sqrt(3.0) / 3.0, 0.0);
+    public readonly Vector2 size;
+    public readonly Vector2 origin;
+    static public Orientation pointy = new Orientation(Mathf.Sqrt(3.0f), Mathf.Sqrt(3.0f) / 2.0f, 0.0f, 3.0f / 2.0f, Mathf.Sqrt(3.0f) / 3.0f, -1.0f / 3.0f, 0.0f, 2.0f / 3.0f, 0.5f);
+    static public Orientation flat = new Orientation(3.0f / 2.0f, 0.0f, Mathf.Sqrt(3.0f) / 2.0f, Mathf.Sqrt(3.0f), 2.0f / 3.0f, 0.0f, -1.0f / 3.0f, Mathf.Sqrt(3.0f) / 3.0f, 0.0f);
 
-    public Point HexToPixel(Hex h)
+    public Vector2 HexToPixel(Hex h)
     {
         Orientation M = orientation;
-        double x = (M.f0 * h.q + M.f1 * h.r) * size.x;
-        double y = (M.f2 * h.q + M.f3 * h.r) * size.y;
-        return new Point(x + origin.x, y + origin.y);
+        float x = (M.f0 * h.q + M.f1 * h.r) * size.x;
+        float y = (M.f2 * h.q + M.f3 * h.r) * size.y;
+        return new Vector2(x + origin.x, y + origin.y);
     }
 
 
-    public FractionalHex PixelToHex(Point p)
+    public FractionalHex PixelToHex(Vector2 p)
     {
         Orientation M = orientation;
-        Point pt = new Point((p.x - origin.x) / size.x, (p.y - origin.y) / size.y);
-        double q = M.b0 * pt.x + M.b1 * pt.y;
-        double r = M.b2 * pt.x + M.b3 * pt.y;
+        Vector2 pt = new Vector2((p.x - origin.x) / size.x, (p.y - origin.y) / size.y);
+        float q = M.b0 * pt.x + M.b1 * pt.y;
+        float r = M.b2 * pt.x + M.b3 * pt.y;
         return new FractionalHex(q, r, -q - r);
     }
 
 
-    public Point HexCornerOffset(int corner)
+    public Vector2 HexCornerOffset(int corner)
     {
         Orientation M = orientation;
-        double angle = 2.0 * Math.PI * (M.start_angle - corner) / 6.0;
-        return new Point(size.x * Math.Cos(angle), size.y * Math.Sin(angle));
+        float angle = 2.0f * (float)Math.PI * (M.start_angle - corner) / 6.0f;
+        return new Vector2(size.x * Mathf.Cos(angle), size.y * Mathf.Sin(angle));
     }
 
 
-    public List<Point> PolygonCorners(Hex h)
+    public List<Vector2> PolygonCorners(Hex h)
     {
-        List<Point> corners = new List<Point> { };
-        Point center = HexToPixel(h);
+        List<Vector2> corners = new List<Vector2> { };
+        Vector2 center = HexToPixel(h);
         for (int i = 0; i < 6; i++)
         {
-            Point offset = HexCornerOffset(i);
-            corners.Add(new Point(center.x + offset.x, center.y + offset.y));
+            Vector2 offset = HexCornerOffset(i);
+            corners.Add(new Vector2(center.x + offset.x, center.y + offset.y));
         }
         return corners;
     }
 
 }
 
-
+#if FALSE
 
 // Tests
-
 
 struct Tests
 {
@@ -442,9 +433,9 @@ struct Tests
     static public void TestLayout()
     {
         Hex h = new Hex(3, 4, -7);
-        Layout flat = new Layout(Layout.flat, new Point(10.0, 15.0), new Point(35.0, 71.0));
+        Layout flat = new Layout(Layout.flat, new Vector2(10.0, 15.0), new Vector2(35.0, 71.0));
         Tests.EqualHex("layout", h, flat.PixelToHex(flat.HexToPixel(h)).HexRound());
-        Layout pointy = new Layout(Layout.pointy, new Point(10.0, 15.0), new Point(35.0, 71.0));
+        Layout pointy = new Layout(Layout.pointy, new Vector2(10.0, 15.0), new Vector2(35.0, 71.0));
         Tests.EqualHex("layout", h, pointy.PixelToHex(pointy.HexToPixel(h)).HexRound());
     }
 
@@ -453,28 +444,28 @@ struct Tests
     {
         Hex a = new Hex(3, 4, -7);
         OffsetCoord b = new OffsetCoord(1, -3);
-        Tests.EqualHex("conversion_roundtrip even-q", a, OffsetCoord.QoffsetToCube(OffsetCoord.EVEN, OffsetCoord.QoffsetFromCube(OffsetCoord.EVEN, a)));
-        Tests.EqualOffsetcoord("conversion_roundtrip even-q", b, OffsetCoord.QoffsetFromCube(OffsetCoord.EVEN, OffsetCoord.QoffsetToCube(OffsetCoord.EVEN, b)));
-        Tests.EqualHex("conversion_roundtrip odd-q", a, OffsetCoord.QoffsetToCube(OffsetCoord.ODD, OffsetCoord.QoffsetFromCube(OffsetCoord.ODD, a)));
-        Tests.EqualOffsetcoord("conversion_roundtrip odd-q", b, OffsetCoord.QoffsetFromCube(OffsetCoord.ODD, OffsetCoord.QoffsetToCube(OffsetCoord.ODD, b)));
-        Tests.EqualHex("conversion_roundtrip even-r", a, OffsetCoord.RoffsetToCube(OffsetCoord.EVEN, OffsetCoord.RoffsetFromCube(OffsetCoord.EVEN, a)));
-        Tests.EqualOffsetcoord("conversion_roundtrip even-r", b, OffsetCoord.RoffsetFromCube(OffsetCoord.EVEN, OffsetCoord.RoffsetToCube(OffsetCoord.EVEN, b)));
-        Tests.EqualHex("conversion_roundtrip odd-r", a, OffsetCoord.RoffsetToCube(OffsetCoord.ODD, OffsetCoord.RoffsetFromCube(OffsetCoord.ODD, a)));
-        Tests.EqualOffsetcoord("conversion_roundtrip odd-r", b, OffsetCoord.RoffsetFromCube(OffsetCoord.ODD, OffsetCoord.RoffsetToCube(OffsetCoord.ODD, b)));
+        Tests.EqualHex("conversion_roundtrip even-q", a, OffsetCoord.QoffsetToCube(OffsetCoord.Even, OffsetCoord.QoffsetFromCube(OffsetCoord.Even, a)));
+        Tests.EqualOffsetcoord("conversion_roundtrip even-q", b, OffsetCoord.QoffsetFromCube(OffsetCoord.Even, OffsetCoord.QoffsetToCube(OffsetCoord.Even, b)));
+        Tests.EqualHex("conversion_roundtrip odd-q", a, OffsetCoord.QoffsetToCube(OffsetCoord.Odd, OffsetCoord.QoffsetFromCube(OffsetCoord.Odd, a)));
+        Tests.EqualOffsetcoord("conversion_roundtrip odd-q", b, OffsetCoord.QoffsetFromCube(OffsetCoord.Odd, OffsetCoord.QoffsetToCube(OffsetCoord.Odd, b)));
+        Tests.EqualHex("conversion_roundtrip even-r", a, OffsetCoord.RoffsetToCube(OffsetCoord.Even, OffsetCoord.RoffsetFromCube(OffsetCoord.Even, a)));
+        Tests.EqualOffsetcoord("conversion_roundtrip even-r", b, OffsetCoord.RoffsetFromCube(OffsetCoord.Even, OffsetCoord.RoffsetToCube(OffsetCoord.Even, b)));
+        Tests.EqualHex("conversion_roundtrip odd-r", a, OffsetCoord.RoffsetToCube(OffsetCoord.Odd, OffsetCoord.RoffsetFromCube(OffsetCoord.Odd, a)));
+        Tests.EqualOffsetcoord("conversion_roundtrip odd-r", b, OffsetCoord.RoffsetFromCube(OffsetCoord.Odd, OffsetCoord.RoffsetToCube(OffsetCoord.Odd, b)));
     }
 
 
     static public void TestOffsetFromCube()
     {
-        Tests.EqualOffsetcoord("offset_from_cube even-q", new OffsetCoord(1, 3), OffsetCoord.QoffsetFromCube(OffsetCoord.EVEN, new Hex(1, 2, -3)));
-        Tests.EqualOffsetcoord("offset_from_cube odd-q", new OffsetCoord(1, 2), OffsetCoord.QoffsetFromCube(OffsetCoord.ODD, new Hex(1, 2, -3)));
+        Tests.EqualOffsetcoord("offset_from_cube even-q", new OffsetCoord(1, 3), OffsetCoord.QoffsetFromCube(OffsetCoord.Even, new Hex(1, 2, -3)));
+        Tests.EqualOffsetcoord("offset_from_cube odd-q", new OffsetCoord(1, 2), OffsetCoord.QoffsetFromCube(OffsetCoord.Odd, new Hex(1, 2, -3)));
     }
 
 
     static public void TestOffsetToCube()
     {
-        Tests.EqualHex("offset_to_cube even-", new Hex(1, 2, -3), OffsetCoord.QoffsetToCube(OffsetCoord.EVEN, new OffsetCoord(1, 3)));
-        Tests.EqualHex("offset_to_cube odd-q", new Hex(1, 2, -3), OffsetCoord.QoffsetToCube(OffsetCoord.ODD, new OffsetCoord(1, 2)));
+        Tests.EqualHex("offset_to_cube even-", new Hex(1, 2, -3), OffsetCoord.QoffsetToCube(OffsetCoord.Even, new OffsetCoord(1, 3)));
+        Tests.EqualHex("offset_to_cube odd-q", new Hex(1, 2, -3), OffsetCoord.QoffsetToCube(OffsetCoord.Odd, new OffsetCoord(1, 2)));
     }
 
 
@@ -536,3 +527,4 @@ struct Tests
     }
 
 }
+#endif
