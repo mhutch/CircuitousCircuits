36a05abc8d5366229ce304b48be87e9af02d70fa
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2019-03-16 01:17:40 -0500

    Create tile images from numeric description

--- Code/PuzzleTileHex.cs
+++ Code/PuzzleTileHex.cs
@@ -1,4 +1,5 @@
-﻿using Godot;
+﻿using System;
+using Godot;
 using Settworks.Hexagons;
 
 public class PuzzleTileHex : Node2D
@@ -15,12 +16,9 @@ public class PuzzleTileHex : Node2D
     {
         base._Ready();
 
-        sprite = new Sprite();
-        sprite.Texture = (Texture)GD.Load("res://Tiles/HexTile.png");
-        var textureHeight = sprite.Texture.GetHeight();
-        float scale = 2f / textureHeight;
-        sprite.Scale = new Vector2(scale, scale);
-        AddChild(sprite);
+        sprite = AddSprite(
+            (Texture)GD.Load("res://Tiles/HexTile.png")
+        );
 
         ZIndex = (int)ZLayers.DroppedTile;
 
@@ -29,20 +27,64 @@ public class PuzzleTileHex : Node2D
         snapTween = new Tween();
         AddChild(snapTween);
 
+        Connections = new[] {
+            //2, 1, 4, 3, 6, 5
+            5, 3, 2, 4, 1, 6
+        };
+
         AddLines();
     }
 
-    void AddLines ()
+    Sprite AddSprite (Texture texture, int zindex = 0)
     {
-        var lines = new Sprite();
-        lines.Texture = (Texture)GD.Load("res://Tiles/largeCurve.png");
-        var textureHeight = lines.Texture.GetHeight();
+        var textureHeight = texture.GetHeight();
         float scale = 2f / textureHeight;
-        lines.Scale = new Vector2(scale, scale);
-        lines.ZAsRelative = true;
-        lines.ZIndex = 1;
-        lines.Modulate = Colors.Aquamarine;
-        AddChild(lines);
+
+        var s = new Sprite
+        {
+            Texture = texture,
+            ZAsRelative = true,
+            ZIndex = zindex,
+            Scale = new Vector2(scale, scale)
+        };
+
+        AddChild(s);
+
+        return s;
+    }
+
+    void AddLines()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            var delta = ((i + 1) - Connections[i] + 6) % 6;
+            if (delta > 3)
+            {
+                continue;
+            }
+
+            Texture texture;
+            switch (delta)
+            {
+                case 0:
+                    texture = (Texture)GD.Load("res://Tiles/stop.png");
+                    break;
+                case 1:
+                    texture = (Texture)GD.Load("res://Tiles/smallCurve.png");
+                    break;
+                case 2:
+                    texture = (Texture)GD.Load("res://Tiles/largeCurve.png");
+                    break;
+                case 3:
+                    texture = (Texture)GD.Load("res://Tiles/line.png");
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+
+            var s = AddSprite(texture, i);
+            s.Rotation = (i - 1) * Mathf.Pi / 3f;
+        }
     }
 
     public void MakeDraggable()
